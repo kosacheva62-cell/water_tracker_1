3344f6f726d58200df8a4c7df978ccee67d5f30a
Татьяна <kosacheva62@google.com>
2026-09-23 18:41:41 +0300

    На странице "Главная" в кнопке "+1 стакан" эмодзи Bubble Tea

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -218,7 +218,8 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
                                   );
                                 }
                               },
-                              text: '+1 стакан',
+                              // ✅ ЗАМЕНЕНО НА BUBBLE TEA БЕЗ ПРОБЕЛА
+                              text: '+1 стакан🧋',
                             ),
                           ),
                           SizedBox(height: spaceAfterButton),
